0ac10ec35c0500c4afb848f26e02526310935512
Xu Kaixuan <triumph_9431@qq.com>
2020-06-05 07:42:15 +0800

    add upload init

--- src/components/Upload/uplaod.stories.tsx
+++ src/components/Upload/uplaod.stories.tsx
@@ -2,16 +2,24 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import Upload from './upload'
+import Icon from '../Icon/icon'
 
-const SimpleUpload = ()=>{
-    return(
-        <Upload
-            action = "http://jsonplaceholder.typicode.com/posts/"
-            onChange={action('changed')}
-            onRemove={action('removed')}
-        />
+const SimpleUpload = () => {
+    return (
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        onChange={action('changed')}
+        onRemove={action('removed')}
+        name="fileName"
+        multiple
+        drag
+      >
+        <Icon icon="upload" size="5x" theme="secondary" />
+        <br/>
+        <p>Drag file over to upload</p>
+      </Upload>
     )
-}
-
-storiesOf('Upload component',module)
-    .add('Upload',SimpleUpload)
+  }
+  
+  storiesOf('Upload component', module)
+    .add('Upload', SimpleUpload)

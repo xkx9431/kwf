e243c9eb7a5fc030b887e2216ae0d80932e50c20
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-31 06:23:55 +0000

    Deploy xkx9431/kwf to github.com/xkx9431/kwf.git:gh-pages

---
+++ precache-manifest.a55ad15970feed7be1f7aa4285ee72c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7ece0ea86fe0c714eab93ee06d2e36",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.95085c645977f36b2cfa.bundle.js"
+  },
+  {
+    "url": "runtime~main.95085c645977f36b2cfa.bundle.js"
+  },
+  {
+    "revision": "35aeee38442cf19489fc",
+    "url": "static/css/main.15cbd38c.chunk.css"
+  },
+  {
+    "revision": "6a1edca6452bfb1d9f4b",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.95085c645977f36b2cfa.bundle.js"
+  },
+  {
+    "url": "vendors~main.95085c645977f36b2cfa.bundle.js.LICENSE.txt"
+  }
+]);
